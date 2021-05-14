e425580817896d114fba3d06aeba039e0c46d0d4
pfxdev <pfxdev@pm.me>
2021-05-15 00:16:52 +1000

    Implement open sesame tag with magic header

--- main.js
+++ main.js
@@ -1,4 +1,57 @@
-// For help writing plugins, visit the documentation to get started:
-//   https://support.insomnia.rest/article/173-plugins
+const tempy = require('tempy')
+const etl = require('etl')
+const mime = require('mime-types')
+const child = require('child_process');
 
-// TODO: Add plugin code here...
+const MAGIC_HEADER_KEY = "OPEN_SESAME"
+const MAGIC_HEADER_VAL = "SUPERCALIFRAGILISTICEXPIALIDOCIOUS_42"
+
+module.exports.templateTags = [{
+    name: 'open_sesame',
+    displayName: 'Open',
+    description: 'Saves response body to a temp file and opens it using system defaults',
+    args: [
+        {
+            displayName: 'Request',
+            type: 'model',
+            model: 'Request',
+        },
+    ],
+    async run(context, requestId) {
+        const request = await context.util.models.request.getById(requestId)
+        request.headers.push({name: MAGIC_HEADER_KEY, value: MAGIC_HEADER_VAL})
+    }
+}]
+
+module.exports.responseHooks = [
+    context => {
+        if (!context.request.getHeader(MAGIC_HEADER_KEY) === MAGIC_HEADER_VAL || context.response.getStatusCode() !== 200) {
+            return
+        }
+
+        const contentType = context.response.getHeader('Content-Type')
+        const extension = mime.extension(contentType)
+        if (!extension) {
+            throw new Error(`Invalid content type ${extension}`)
+        }
+
+        const tempFile = tempy.file({extension: extension})
+        context.response.getBodyStream()
+            .pipe(etl.toFile(tempFile))
+            .promise()
+            .then(() => child.exec(getCommandLine() + ' ' + tempFile))
+    }
+]
+
+function getCommandLine() {
+    switch (process.platform) {
+        case 'darwin' :
+            return 'open';
+        case 'win32' :
+            return 'start';
+        case 'win64' :
+            return 'start';
+        default :
+            return 'xdg-open';
+    }
+}
